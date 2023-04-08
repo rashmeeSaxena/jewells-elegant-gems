30dc06674f26c20df60ee5c6ad6c6dbad77c971e
Rashmee Saxena <rashmee.saxena@gmail.com>
2023-04-09 00:58:16 +0100

    modulename error fix2

--- Jewells/settings.py
+++ Jewells/settings.py
@@ -133,11 +133,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
-'/static',
+'Jewells/static',
 ]
 
 
-
+STATICFILES_DIRS = [
+    'Jewells/static',
+]
 #STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
