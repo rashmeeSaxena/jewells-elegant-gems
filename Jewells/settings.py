ade01b1e27a2c94a4c52ecd7e81a6f5146db4ded
Rashmee Saxena <rashmee.saxena@gmail.com>
2023-04-09 01:57:29 +0100

    SQLite to Postgresql3

--- Jewells/settings.py
+++ Jewells/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
 
 import dj_database_url
 dj_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(dj_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
