3566b25bbd15eba9d41c82066d30bc947ddbc321
Rashmee Saxena <rashmee.saxena@gmail.com>
2023-04-09 01:23:14 +0100

    views.py

--- Jewells/views.py
+++ Jewells/views.py
@@ -3,7 +3,7 @@ from store.models import Product
 #, ReviewRating
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')
     #.order_by('created_date')
 
     # Get the reviews
