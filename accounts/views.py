438d2d840027fa1e8995751f840a8f490ec97d55
Rashmee <rashmee.saxena@gmail.com>
2023-04-25 02:46:21 +0100

    comment a message

--- accounts/views.py
+++ accounts/views.py
@@ -110,7 +110,7 @@ def login(request):
             except:
                 pass
             auth.login(request, user)
-            messages.success(request, 'You are now logged in.')
+            #messages.success(request, 'You are now logged in.')
             #url = request.META.get('HTTP_REFERER')
             #try:
             #    query = requests.utils.urlparse(url).query
